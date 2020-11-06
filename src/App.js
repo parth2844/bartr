a9f18c4c8a4dba0e8c9eaa344e62668435ee106d
Parth Shah <parth2844@gmail.com>
2020-11-05 18:21:16 -0800

    SetUp for Landing and Error Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Landing from './components/pages/landing'
+import Error from './components/pages/error'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>  save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (    
+    <Router>    
+        
+      <Switch>
+        <Route path="/" exact component={Landing} />        
+        <Route component={Error} />
+      </Switch>
+      
+    </Router>    
   );
 }
 
